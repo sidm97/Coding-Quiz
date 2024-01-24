406b5170aff6dd489c9e727dd1f89def6e6d8c64
sidm97 <sid.m.974@gmail.com>
2024-01-24 14:41:58 +0000

    added pseudocode for timer element and countdown function

---
+++ assets/js/logic.js
@@ -0,0 +1,9 @@
+// when start button pressed, timer should start
+// id = start (create event listener)
+
+// create multiple convenience variables
+// 	create variable secsleft that starts at 60
+// 	create selector for id = time and set default as 0 when not started
+
+// create function that changes text of "time", count --, every 1000
+

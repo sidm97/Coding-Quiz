8f1bb247305d5ff7749a5d4873bf3c494b926d5a
sidm97 <sid.m.974@gmail.com>
2024-01-26 16:55:11 +0000

    updated questions, shamelessly stolen from W3school

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -11,30 +11,30 @@
 // numbers
 
 var questionOne = {
-  title: "1. commonly used data types do not include: ",
-  choices: ["strings", "booleans","alerts", "numbers"],
-  correct: "alerts"
+  title: "1. Commonly used data types do NOT include: ",
+  choices: ["Strings", "Booleans","Alerts", "Numbers"],
+  correct: "Alerts"
 }
 var questionTwo = {
-  title: "2. commonly used data types do not include: ",
-  choices: ["2strings", "1booleans","1alerts", "1numbers"],
-  correct: "1alerts"
+  title: "2. Inside which HTML element do we put the JavaScript?",
+  choices: ["<scripting>", "<javascript>","<js>", "<script>"],
+  correct: "<js>"
 }
 
 var questionThree = {
-  title: "3. commonly used data types do not include: ",
-  choices: ["3strings", "1booleans","1alerts", "1numbers"],
-  correct: "1alerts"
+  title: "3. Where is the correct place to insert a JavaScript?",
+  choices: ["The <head section>", "The <body> section","Both the <head> and <body> sections"],
+  correct: "The <body> section"
 }
 var questionFour = {
-  title: "4. commonly used data types do not include: ",
-  choices: ["4strings", "1booleans","1alerts", "1numbers"],
-  correct: "1alerts"
+  title: "4. How do you write 'Hello World!' in an alert box?",
+  choices: ["alertBox('Hello World!)", "msgBox('Hello World!)","msg('Hello World!)", "alert('Hello World!)"],
+  correct: "alert('Hello World!)"
 }
 var questionFive = {
-  title: "5. commonly used data types do not include: ",
-  choices: ["5strings", "1booleans","1alerts", "1numbers"],
-  correct: "1alerts"
+  title: "5. How does a FOR loop start?",
+  choices: ["for(i = 0; i <= 5; i++)", "for(i = 0; i <= 5)","for (i <= 5; i++)", "for i = 1 to 5"],
+  correct: "for(i = 0; i <= 5"
 }
 
 

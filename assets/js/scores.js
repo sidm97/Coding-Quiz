38b09b315a0908e4d4ba470bcb2a75d2105426bd
sidm97 <sid.m.974@gmail.com>
2024-01-27 16:56:40 +0000

    coded to arrange highscores in descending order

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,22 +2,38 @@ highscoreEl = document.querySelector("#highscores")
 
 var highscoreEntries = {...localStorage};
 
+// console.log(highscoreEntries);
 
-for (const key in highscoreEntries) {
+// for (const key in highscoreEntries) {
+//     var li = document.createElement("li");
+//     var entry = `${key}: ${highscoreEntries[key]}`
+//     li.textContent = entry;
+//     console.log(`${key}: ${highscoreEntries[key]}`);
+//     highscoreEl.appendChild(li);
+// }
+
+let sortable = [];
+for (var key in highscoreEntries) {
+    sortable.push([key, highscoreEntries[key]]);
+}
+// console.log(sortable);
+sortable.sort(function(a, b) {
+    return a[1] - b[1];
+});
+sortable.reverse();
+// console.log(sortable);
+
+for (let i = 0; i < sortable.length; i++) {
+    const element = sortable[i];
     var li = document.createElement("li");
-    var entry = `${key}: ${highscoreEntries[key]}`
+    var entry = element[0] + "-----" + element[1]
     li.textContent = entry;
-    console.log(`${key}: ${highscoreEntries[key]}`);
     highscoreEl.appendChild(li);
 }
 
+// Explanation of above; first, the local storage is sourced and stored in an object. An array is created and filled with the entries of the object; in this case, each entry is a key and value, a user and score. the new array or arrays is sorted in descending value order and a for loop renders it onto the page.
 
-
-
-
-
-
-
+console.log(Object.keys(highscoreEntries));
 
 
 
